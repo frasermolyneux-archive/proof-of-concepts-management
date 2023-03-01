e924c666b9321e1b4b91495334e94ae024fce39e
Fraser Molyneux <fmolyneux@microsoft.com>
2023-03-01 15:53:47 +0000

    Adding azure devops provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.35.0"
     }
+    azuredevops = {
+      source  = "microsoft/azuredevops"
+      version = ">=0.1.0"
+    }
     github = {
       source  = "integrations/github"
       version = "~> 5.0"
